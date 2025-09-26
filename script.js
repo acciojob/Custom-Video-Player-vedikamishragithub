d22cd883df2d2d94ff35f871a1b1b4b826e211bc
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-09-27 00:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-/* Edit this file */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
+const toggle = player.querySelector('.toggle'); //done
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+video.addEventListener('timeupdate', () => {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = `${percent}%`;
+  progressBar.style.flexBasis = `${percent}%`;
+});
+
+// Seek video on click
+progress.addEventListener('click', (e) => {
+  const rect = progress.getBoundingClientRect();
+  const clickX = e.clientX - rect.left;
+  const width = rect.width;
+  let newTime = (clickX / width) * video.duration;
+  newTime = Math.max(0, Math.min(newTime, video.duration));
+  video.currentTime = newTime;  
+});
+
+// Toggle play/pause
+toggle.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+    toggle.innerText = '❚ ❚';
+  } else {
+    video.pause();
+    toggle.innerText = '►';
+  }
+});
+
+ranges.forEach(range=>{
+	
+	range.addEventListener('input',()=>{
+		if(range.name==='volume'){
+			video.volume=range.value;
+		}else if(range.name==='playbackRate'){
+			const newSpeed=parseFloat(range.value);
+	        video.playbackRate = parseFloat(range.value);
+		}	
+});
+});
+skipButtons.forEach(skipButton=>{
+	skipButton.addEventListener('click',()=>{
+       const skipAmount=parseFloat(skipButton.dataset.skip);
+	   video.currentTime+=skipAmount;	
+	});
+});
+
